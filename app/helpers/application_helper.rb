4652a18c4376ba1b1abf5fdc9714b4080884d213
Victor <victorlaucas@gmail.com>
2017-11-09 14:24:41 -0700

    Put navbar ruby logic inside of helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def sign_in user
+    if current_user 
+      (link_to "Logout", destroy_user_session_path, method: :delete)
+     else 
+      (link_to "Sign In", new_user_session_path) + " ".html_safe +
+      (link_to "Register", new_user_registration_path)
+    end 
+  end
 end
