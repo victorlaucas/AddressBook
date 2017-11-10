a7b6318bdcf4d85b0626a3cbfac24d5eacfeac84
Victor <victorlaucas@gmail.com>
2017-11-09 21:29:32 -0700

    Added email and phone number regex validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,13 @@
 class Contact < ApplicationRecord
-  validates_presence_of :name, :address, :email, :phone 
+  validates_presence_of :name, :address, :email, :phone
+  EMAIL_REGEX = /\A\S+@.+\.\S+\z/
+
+  validates_format_of :email, with: EMAIL_REGEX 
+
+  PHONE_REGEX = /\A[0-9]*\Z/
+
+  validates_format_of :phone, with: PHONE_REGEX
+
+  validates :phone, length: { is: 10 } 
   scope :sort_by_name, -> { order('name ASC') }
 end
