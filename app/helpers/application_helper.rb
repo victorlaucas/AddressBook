83edbd4d720332c17d9490d3f06f6e7a82ca1a3f
Victor <victorlaucas@gmail.com>
2017-11-11 12:27:38 -0700

    Had to remove ruby logic from helper method to fix bug in nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-  def sign_in user
-    if current_user 
-      (link_to "Logout", destroy_user_session_path, method: :delete)
-     else 
-      (link_to "Sign In", new_user_session_path) + " ".html_safe +
-      (link_to "Register", new_user_registration_path)
-    end 
-  end
-
   def admin_dash user
     if logged_in?(:admin)
       (link_to "Admin Dashboard", admin_root_path)
